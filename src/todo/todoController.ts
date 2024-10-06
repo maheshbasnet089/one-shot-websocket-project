a15f8c90b66de6d231855d204f90c51b23f9ea33
maheshbasnet089 <basnetmanish089@gmail.com>
2024-10-06 23:31:03 +0545

    add handleAddTodo

--- src/todo/todoController.ts
+++ src/todo/todoController.ts
@@ -1,3 +1,37 @@
+import { Socket } from "socket.io";
 import { getSocketIo } from "../../server";
+import todoModel from "./todoModel";
 
-const io = getSocketIo()
+class Todo{
+    private io = getSocketIo(); 
+    constructor(){
+        this.io.on("connection",(socket:Socket)=>{
+            console.log("new client connected !!")
+            socket.on("addTodo",(data)=>this.handleAddTodo(socket,data))
+        })
+    }
+    private async handleAddTodo(socket:Socket,data:any){
+        try {
+        const {task,deadLine,status} = data
+        await todoModel.create({
+            task, 
+            deadLine, 
+            status
+        })
+        const todos = await todoModel.find()
+        socket.emit("todos_updated",{
+            status : "success", 
+            data : todos
+        })
+        
+        } catch (error) {
+            socket.emit("todo_response",{
+                status : "error", 
+                error
+            })
+        }
+    }
+}
+
+export default new Todo()
+
