27cfa57e602354e8417f4d91f75cbfd93d4a0d98
maheshbasnet089 <basnetmanish089@gmail.com>
2024-10-06 22:19:29 +0545

    db connect

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import app from './src/app'
 import { envConfig } from './src/config/config'
+import connectToDb from './src/config/db'
 
 
 function startServer(){
+     connectToDb()
     const port = envConfig.port || 4000
     app.listen(port,()=>{
         console.log(`Server has started at port[${port}]`)
--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,8 @@ import { config } from "dotenv";
 config()
 
 export const envConfig = {
-    port : process.env.PORT
+    port : process.env.PORT, 
+    mongoConnectionString : process.env.MONGO_URI
 }
 
 
