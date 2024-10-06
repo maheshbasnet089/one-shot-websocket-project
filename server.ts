0ed71d51d143c0dfb34f4666c6ed68b2b49e6cf8
maheshbasnet089 <basnetmanish089@gmail.com>
2024-10-06 23:07:27 +0545

    learn basics ws

--- server.ts
+++ server.ts
@@ -1,14 +1,28 @@
 import app from './src/app'
 import { envConfig } from './src/config/config'
 import connectToDb from './src/config/db'
+import {Server} from 'socket.io'
 
+// data receive garda - on 
+// data pathauda  - emit 
+// request -- socket 
+// api -- event
+// req.body = data
 
 function startServer(){
      connectToDb()
     const port = envConfig.port || 4000
-    app.listen(port,()=>{
+    const server = app.listen(port,()=>{
         console.log(`Server has started at port[${port}]`)
     })
+    const io = new Server(server)
+    io.on("connection",(socket)=>{
+       socket.emit("message",{
+        name : "Manish"
+       })
+        console.log("Someone connected (client)")
+    })
+    
 }
 
 startServer()
