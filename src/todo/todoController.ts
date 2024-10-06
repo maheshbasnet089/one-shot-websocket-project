61a0ecf43ac838f464182f45d216a44a39730b2c
maheshbasnet089 <basnetmanish089@gmail.com>
2024-10-06 23:37:52 +0545

    add handleDeleteTodo

--- src/todo/todoController.ts
+++ src/todo/todoController.ts
@@ -1,6 +1,7 @@
 import { Socket } from "socket.io";
 import { getSocketIo } from "../../server";
 import todoModel from "./todoModel";
+import { ITodo } from "./todoTypes";
 
 class Todo{
     private io = getSocketIo(); 
@@ -8,9 +9,10 @@ class Todo{
         this.io.on("connection",(socket:Socket)=>{
             console.log("new client connected !!")
             socket.on("addTodo",(data)=>this.handleAddTodo(socket,data))
+            socket.on("deleteTodo",(data)=>this.handleDeleteTodo(socket,data))
         })
     }
-    private async handleAddTodo(socket:Socket,data:any){
+    private async handleAddTodo(socket:Socket,data:ITodo){
         try {
         const {task,deadLine,status} = data
         await todoModel.create({
@@ -30,6 +32,30 @@ class Todo{
                 error
             })
         }
+    }
+    private async handleDeleteTodo(socket:Socket,data:{id:string}){
+       try {
+        const {id} = data 
+        const deletedTodo = await todoModel.findByIdAndDelete(id)
+        if(!deletedTodo){
+            socket.emit("todo_response",{
+                status : "error", 
+                message : "Todo not found"
+            })
+            return;
+        }
+        const todos = await todoModel.find()
+        socket.emit("todos_updated",{
+            status : "success", 
+            data : todos
+        })
+       } catch (error) {
+        socket.emit("todo_response",{
+            status : "error", 
+            error
+        })
+       }
+
     }
 }
 
